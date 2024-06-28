07734c04909dcf1ba82c84af233fb7da9adaae94
Grameco <113364758+Grameco@users.noreply.github.com>
2024-06-28 15:08:06 +0530

    Increment Decrement

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [count, handleCounter] = useState(0);
+
+  function decrease() {
+    handleCounter(count - 1);
+    console.log("subtracted")
+  }
+
+  function increase() {
+    handleCounter(count + 1);
+    console.log("added")
+  }
+
+  function reset() {
+    handleCounter(0);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col gap-10 w-full min-h-[100vh] bg-[#282c34] justify-center items-center">
+      <h1 className="text-[#0398d4] text-[3rem]">Increment and Decrement</h1>
+      <div className="flex justify-center gap-12 py-3 rounded-sm font-semibold text-[2rem] text-[#344151] bg-white">
+        <button onClick={decrease} className="w-20 border-r-2 text-center border-[#bfbfbf]">-</button>
+        <div className='text-[2rem] font-semibold'>{count}</div>
+        <button onClick = {increase} className="w-20 border-l-2 text-center border-[#bfbfbf]">+</button>
+      </div>
+      <button onClick = {reset} className="text-white bg-[#0398d4] px-5 py-2 rounded-sm text-lg" >Reset</button>
     </div>
   );
 }
